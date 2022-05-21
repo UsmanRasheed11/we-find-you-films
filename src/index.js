76a844f4845185ff5a42c45a74d1c89173ae6827
usm <usm@usm.usm>
2022-05-21 08:43:49 +0500

    fix login signUp issues

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import "./_theme/css/bootstrap.min.css";
 import "./_theme/fonts/icomoon/style.css";
 import App from "./app/App";
 import "./index.css";
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+// import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 const { PUBLIC_URL } = process.env;
 
